42a89e3203185acf2fefa1114bb034fdcfc0e9e0
2017463 <73094946+2017463@users.noreply.github.com>
2020-11-12 21:25:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hamza jahangir 2017463
         </a>
       </header>
     </div>
